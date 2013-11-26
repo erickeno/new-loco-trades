fbfdcdea534266916135b3de3d7736491018bdc1
erick bett <erickeno@yahoo.com>
2013-11-26 17:01:04 -0500

    more changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -14,15 +14,14 @@ require 'securerandom'
 def secure_token
   token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
-  	#Use the existing token
-  	File.read(token_file).chomp
+    # Use the existing token.
+    File.read(token_file).chomp
   else
-  	#Generate a nuew token and store it in token_file.
-  	token = secureRandom.hex(64)
-  	File.write(token_file, token)
-  	token
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
   end
 end
 
-
-LocoTrades::Application.config.secret_key_base = secure_token
+SampleApp::Application.config.secret_key_base = secure_token
