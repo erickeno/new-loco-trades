dd7e3096e5965cc16c39f680193e110e0e90024a
erick bett <erickeno@yahoo.com>
2013-11-26 16:33:11 -0500

    First commit and some changes to the .gitignore and gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocoTrades::Application.config.secret_key_base = '863b9b8865d7991dbe375e503b14877b7ab10d1f07c66a7889ecb5a20590e7d791ff330e239f0a4c1c90b08dab41e6844bc1cde13ef39f7451aaaff5a6050442'
